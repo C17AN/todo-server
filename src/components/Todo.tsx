37692f42948a124603a891e11137bddf65e90fdf
c17an <kimchanmin1@gmail.com>
2020-08-07 13:06:30 +0900

    태스크 등록, 삭제 기능 구현

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ReactElement } from "react";
+import React, { useReducer, useState, ReactElement } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
@@ -15,18 +15,60 @@ type Task = {
   index: number;
 };
 
+type Action = { type: "ADD"; task: Task } | { type: "DELETE"; index: number };
+
+function reducer(tasks: Task[], action: Action): Task[] {
+  switch (action.type) {
+    case "ADD":
+      return [...tasks, action.task];
+    case "DELETE":
+      // 삭제 시, Task 목록의 인덱스 조정함.
+      // e.g. {0, 1, 2} 중 인덱스 1이 삭제되면 2가 1로 오게끔
+      return tasks
+        .filter((el) => el.index !== action.index)
+        .map((el, index) => ({ ...el, index: index }));
+    default:
+      throw new Error("unhandled action");
+  }
+}
+
 interface Props {}
 
-function reducer(state: Task[], action: any) {}
-
 export default function Todo({}: Props): ReactElement {
-  const [tasks, setTask] = useState<Task[]>([
+  const [tasks, dispatch] = useReducer(reducer, [
     { title: "밥먹기", isComplete: true, index: 0 },
     { title: "코딩하기", isComplete: true, index: 1 },
     { title: "아에이오우아아", isComplete: true, index: 2 },
   ]);
+  // task 초기상태
+  const [task, setTask] = useState({ title: "", isComplete: false, index: 0 });
+
+  // addTask 는 생성한 task 를 task 목록에 추가하는 역할
+  const addTask = (): void => dispatch({ type: "ADD", task: task });
+  // deleteTask 는 삭제할 태스크를 전달해야 함.
+  const deleteTask = (index: number): void => {
+    console.log(tasks);
+    console.log(index);
+    dispatch({ type: "DELETE", index: index });
+  };
+
+  // 입력창에 타이핑 될때마다 호출됨
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setTask({
+      title: e.target.value,
+      isComplete: false,
+      index: tasks.length,
+    });
+  };
+  // 폼 제출시
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    e.persist();
+    console.log("hi");
+  };
 
   return (
+    // 타이틀
     <div className="main__container">
       <div className="title__container">
         <h1>To Do List</h1>
@@ -35,23 +77,34 @@ export default function Todo({}: Props): ReactElement {
           <div className="text__dark-mode">다크 모드</div>
         </div>
       </div>
+      {/* 입력바 부분 */}
       <div className="main__header">
-        <div className="inputBar">
+        <form className="inputBar" onSubmit={handleSubmit}>
           <input
             className="task__input"
             placeholder="할일을 입력해주세요"
+            value={task.title}
+            onChange={handleChange}
           ></input>
-          <button className="task__button">등록</button>
-        </div>
+          <button type="submit" className="task__button" onClick={addTask}>
+            등록
+          </button>
+        </form>
       </div>
       {/* 상태바 컴포넌트 */}
       <TaskStatus></TaskStatus>
       {/* 태스크 목록 컴포넌트 */}
       <div className="todos__container">
-        {tasks.map((el) => (
-          <TodoList title={el.title}></TodoList>
+        {/* 미리 태스크의 인덱스를 삭제 함수에 넘김. */}
+        {tasks.map((el: Task, index) => (
+          <TodoList
+            title={el.title}
+            onComplete={() => deleteTask(index)}
+            key={index}
+          ></TodoList>
         ))}
       </div>
+      {/* footer 부분 */}
       <footer>
         {" "}
         <div className="icon__container">
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,20 @@
 import React, { ReactElement } from "react";
-import TodoItem from "./TodoItem";
 
 interface Props {
   title: string;
+  onComplete: () => void;
 }
 
-export default function TodoList({ title }: Props): ReactElement {
+export default function TodoList({ title, onComplete }: Props): ReactElement {
   return (
     <ul>
       <div className="task">
         <div className="task__title">{title}</div>
         <div className="task__button__container">
           <button className="task__complete">시작</button>
-          <button className="task__complete">완료</button>
+          <button className="task__complete" onClick={onComplete}>
+            완료
+          </button>
         </div>
       </div>
     </ul>
